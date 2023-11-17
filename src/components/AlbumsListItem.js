522355d9c4f1f0d2f02d1680ccd7fba6a357f990
Ajay <ajaysajikumar8@gmail.com>
2023-11-17 18:14:02 +0530

    first commit

---
+++ src/components/AlbumsListItem.js
@@ -0,0 +1,26 @@
+import { useRemoveAlbumMutation } from "../store";
+import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
+import PhotosList from "./PhotosList";
+import { GoTrash } from "react-icons/go";
+
+function AlbumListItem({album}) {
+    const [removeAlbum, results] = useRemoveAlbumMutation()
+
+    const handleRemoveAlbum = () => {
+        removeAlbum(album)
+    }
+
+    const header = <>
+        <Button className="mr-2" onClick={handleRemoveAlbum} loading={results.isLoading}><GoTrash/></Button>
+        {album.title}
+    </>;
+
+    return (
+        <ExpandablePanel key={album.id} header={header}>
+            <PhotosList album={album}/>
+        </ExpandablePanel>
+    );
+}
+
+export default AlbumListItem
---
+++ src/components/PhotosListItem.js
@@ -0,0 +1,27 @@
+import { GoTrash } from "react-icons/go";
+import { useRemovePhotoMutation } from "../store";
+import Button from "./Button";
+
+function PhotosListItem({ photo }) {
+	const [removePhoto, results] = useRemovePhotoMutation(photo);
+
+	const handleRemovePhoto = () => {
+		removePhoto(photo);
+	};
+
+	return (
+		<div
+			onClick={handleRemovePhoto}
+			className="relative cursor-pointer m-2"
+		>
+			<img className="h-20 w-20" src={photo.url} alt="random pic" />
+			<div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
+				<Button loading={results.isLoading}>
+					<GoTrash className="text-3xl" />
+				</Button>
+			</div>
+		</div>
+	);
+}
+
+export default PhotosListItem;
